6729a9dd5aa03940258775bd29c5acde88da5e1a
Gusta <gustavo.rodrigues2005@outlook.com>
2026-08-10 21:24:43 -0300

    feat: variaveis com LET

---
+++ aula6/index.js
@@ -0,0 +1,12 @@
+// Não podemos criar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+// Não pode começar a variavel com número
+// Não pode conter espaços vazios 
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilizar VAR, utilizar LET
+let nomeCliente = 'Luiz';
+let nomecliente = 'Henrique';
+
+console.log(nomeCliente, nomecliente);
