b947b84862aaa92ea8d27fd64596d0e3429d0310
Gusta <gustavo.rodrigues2005@outlook.com>
2026-08-14 14:14:03 -0300

    fix: correcao de nome de variavel

--- aula8/index.js
+++ aula8/index.js
@@ -8,9 +8,9 @@ const sobrenome = 'Miranda';
 const idade = 30;
 const peso = 84;
 const alturaEmM = 1.80;
-let imc = peso / (alturaEmM * alturaEmM); // Peso / (alturaEmM * alturaEmM)
+let indiceMassaCorporal = peso / (alturaEmM * alturaEmM); // Peso / (alturaEmM * alturaEmM)
 let anoNascimento = 2026 - idade;
 
 console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso, 'kg')
-console.log('tem', alturaEmM, 'de alturaEmM e seu IMC é de', imc)
+console.log('tem', alturaEmM, 'de alturaEmM e seu IMC é de', indiceMassaCorporal)
 console.log(nome, 'nasceu em', anoNascimento)
