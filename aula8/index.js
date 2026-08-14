7dee9210d90e2f88e3b6483e2ababcc746e53aab
Gusta <gustavo.rodrigues2005@outlook.com>
2026-08-14 14:03:27 -0300

    docs: exercicio

---
+++ aula8/index.js
@@ -0,0 +1,16 @@
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de alturaEmM e seu IMC é de 25.925
+Luiz Otávio nasceu em 1980
+*/
+const nome = 'Luiz Otávio';
+const sobrenome = 'Miranda';
+const idade = 30;
+const peso = 84;
+const alturaEmM = 1.80;
+let imc = peso / (alturaEmM * alturaEmM); // Peso / (alturaEmM * alturaEmM)
+let anoNascimento = 2026 - idade;
+
+console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso, 'kg')
+console.log('tem', alturaEmM, 'de alturaEmM e seu IMC é de', imc)
+console.log(nome, 'nasceu em', anoNascimento)
